8a239a08c241c2c89bebfc108c188a3faa219d4d
smirnovsky <happy280190@gmail.com>
2023-01-06 15:06:03 +0300

    add: add search component

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f6c1e33860msh4b193527828127bp100cf9jsn8fcf1ff22f1d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
