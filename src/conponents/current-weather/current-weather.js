4028d63b1224abbb1cfba818b5e74d9a3d412330
smirnovsky <happy280190@gmail.com>
2023-01-07 13:38:04 +0300

    feat: add API data for weather in current-weather component

--- src/conponents/current-weather/current-weather.js
+++ src/conponents/current-weather/current-weather.js
@@ -17,22 +17,22 @@ const CurrentWeather = ({data}) => {
                     </div>
                     <div className='parameter-row'>
                         <span className='parameter-label'>Feels like</span>
-                        <span className='parameter-value'>22°C</span>
+                        <span className='parameter-value'>{Math.round(data.main.feels_like)}°C</span>
                     </div>
                     <div className='parameter-row'>
                         <span className='parameter-label'>Wind</span>
-                        <span className='parameter-value'>4 m/s</span>
+                        <span className='parameter-value'>{data.wind.speed} m/s</span>
                     </div>
                     <div className='parameter-row'>
                         <span className='parameter-label'>Humidity</span>
-                        <span className='parameter-value'>20%</span>
+                        <span className='parameter-value'>{data.main.humidity}%</span>
                     </div>
                     <div className='parameter-row'>
                         <span className='parameter-label'>Pressure</span>
-                        <span className='parameter-value'>20 hPa</span>
+                        <span className='parameter-value'>{data.main.pressure} hPa</span>
                     </div>
                 </div>
-                <p className='temperature'>18°C</p>
+                <p className='temperature'>{Math.round(data.main.temp)}°C</p>
             </div>
         </div>
     );
